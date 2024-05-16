4b7a658f16c4fef4f90729b5ead333bdea713f74
OskarBelza <oskar.belza@edu.uekat.pl>
2024-05-16 14:25:42 +0200

    button css change

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -61,19 +61,21 @@ function ContactPage() {
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
+                    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
+                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange}/>
                 </div>
                 <div>
                     <label htmlFor="message">Message</label>
                     <textarea id="message" name="message" value={formData.message} onChange={handleChange}></textarea>
                 </div>
-                <button type="submit">Send</button>
+                <div className="button-container">
+                    <button type="submit">Send</button>
+                </div>
             </form>
-            <ToastContainer />
+            <ToastContainer/>
         </div>
     );
 }
